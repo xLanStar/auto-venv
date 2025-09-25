66b0f00c739d200a2b34ad83b320ffe3dd2eadf4
xLanStar <danny95624268@gmail.com>
2025-09-25 16:54:58 +0800

    feat: auto venv on startup and only in interactive

--- conf.d/venv.fish
+++ conf.d/venv.fish
@@ -1,14 +1,18 @@
-function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
-    status --is-command-substitution; and return
-    if git rev-parse --show-toplevel &>/dev/null
-        set gitdir (realpath (git rev-parse --show-toplevel))
-        set cwd (pwd -P)
-        if [ -e "$cwd/.venv/bin/activate.fish" ]
-            . "$cwd/.venv/bin/activate.fish" &>/dev/null
-            return
+if status is-interactive
+    function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
+        status --is-command-substitution; and return
+        if git rev-parse --show-toplevel &>/dev/null
+            set gitdir (realpath (git rev-parse --show-toplevel))
+            set cwd (pwd -P)
+            if [ -e "$cwd/.venv/bin/activate.fish" ]
+                . "$cwd/.venv/bin/activate.fish" &>/dev/null
+                return
+            end
+        end
+        if [ -n "$VIRTUAL_ENV" ]
+            deactivate
         end
     end
-    if [ -n "$VIRTUAL_ENV" ]
-        deactivate
-    end
+
+    __auto_source_venv $(builtin pwd -P)
 end
